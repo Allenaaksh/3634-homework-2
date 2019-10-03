887db0c0a0e8d3dd9d5b802ef6711bcddf2e60ef
mayuxiang0929 <z5142200@ad.unsw.edu.au>
2019-10-03 22:10:46 +1000

    the hide city component will represented as weather details  for this city and can back by click the 'back'button

--- app/src/main/java/com/example/shinelon/hw/MainActivity.java
+++ app/src/main/java/com/example/shinelon/hw/MainActivity.java
@@ -1,69 +1,138 @@
 package com.example.shinelon.hw;
 
-import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextClock;
 import android.widget.TextView;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView textView1;
-    TextView textView2;
-    TextView textView3;
-    TextView textView4;
-    TextView textView5;
-     Button button;
+    TextClock clock1;
+    TextClock clock2;
+    TextClock clock3;
+    TextClock clock4;
+    TextClock clock5;
+
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        clock1= findViewById(R.id.clock1) ;
+        clock2= findViewById(R.id.clock2) ;
+        clock3= findViewById(R.id.clock3) ;
+        clock4= findViewById(R.id.clock4) ;
+        clock5= findViewById(R.id.clock5) ;
+        Button buttonswitch1 = findViewById(R.id.switch1);
+        Button buttonswitch2 = findViewById(R.id.switch2);
+        Button button1 = findViewById(R.id.button1);
+        Button button2 = findViewById(R.id.button2);
+        Button button3 = findViewById(R.id.button3);
+        Button button4 = findViewById(R.id.button4);
+        Button button5 = findViewById(R.id.button5);
 
 
+        TextView textView1 =findViewById(R.id.move1);
 
-        textView1 = findViewById(R.id.time1);
-        textView2 = findViewById(R.id.time2);
-        textView3 = findViewById(R.id.time3);
-        textView4 = findViewById(R.id.time4);
-        textView5 = findViewById(R.id.time5);
-        button = findViewById(R.id.update1);
-        button = findViewById(R.id.update2);
-        button = findViewById(R.id.update3);
-        button = findViewById(R.id.update4);
-        button = findViewById(R.id.update5);
-       // button = findViewById(R.id.update1);
-       // button = findViewById(R.id.update1);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        buttonswitch1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Calendar calendar = Calendar.getInstance();
-                DateFormat dateFormat =DateFormat.getDateTimeInstance();
-                String string = dateFormat.format(dateFormat);
 
-                SimpleDateFormat simpleDateFormat = new SimpleDateFormat(" hh:mm:ss a");
-                String dateTime = simpleDateFormat.format(calendar.getTime());
-                textView1.setText(string);
-                textView2.setText(dateTime);
-                textView3.setText(dateTime);
-                textView4.setText(dateTime);
-                textView5.setText(dateTime);
-
-
+                clock1.setFormat12Hour("HH:mm:ss");
+                clock2.setFormat12Hour("HH:mm:ss");
+                clock3.setFormat12Hour("HH:mm:ss");
+                clock4.setFormat12Hour("HH:mm:ss");
+                clock5.setFormat12Hour("HH:mm:ss");
 
 
             }
-            });
+        });
+
+        buttonswitch2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                clock1.setFormat12Hour("hh:mm:ss a");
+                clock2.setFormat12Hour("hh:mm:ss a");
+                clock3.setFormat12Hour("hh:mm:ss a");
+                clock4.setFormat12Hour("hh:mm:ss a");
+                clock5.setFormat12Hour("hh:mm:ss a");
+
+            }
+        });
+
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =new Intent(getApplicationContext(), beijingmain.class);
+                //intent.putExtra("article", 1);
+                startActivity(intent);
+
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =new Intent(getApplicationContext(), brisbenmain.class);
+                //intent.putExtra("article", 1);
+                startActivity(intent);
+
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =new Intent(getApplicationContext(), melbmain.class);
+                //intent.putExtra("article", 1);
+                startActivity(intent);
+
+            }
+        });
+
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =new Intent(getApplicationContext(), sydneymain.class);
+                //intent.putExtra("article", 1);
+                startActivity(intent);
+
+            }
+        });
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =new Intent(getApplicationContext(), tykomain.class);
+                //intent.putExtra("article", 1);
+                startActivity(intent);
+
+            }
+        });
+
+
+
+
+
+
+
+
+
     }
-        }
+}
 
 
 
---
+++ app/src/main/java/com/example/shinelon/hw/beijingmain.java
@@ -0,0 +1,34 @@
+package com.example.shinelon.hw;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toolbar;
+
+/**
+ * Created by Shinelon on 3/10/2019.
+ */
+
+public class beijingmain extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.beijingmain);
+
+        ImageButton imageButton =findViewById(R.id.imageButton1);
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =new Intent(getApplicationContext(), MainActivity.class);
+                //intent.putExtra("article", 1);
+                startActivity(intent);
+            }
+        });
+
+
+
+
+    }
+}
